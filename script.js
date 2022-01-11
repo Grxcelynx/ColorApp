7ad73579b80b369e770425b6dd1d6753d609ee47
Grace Birnam <grace.mbirnam@gmail.com>
2022-01-11 14:54:32 -0800

    started add image feature - base working

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const colorize = document.getElementById("colorize");
 const color1 = document.getElementById("pallete1");
 const color2 = document.getElementById("pallete2");
 const color3 = document.getElementById("pallete3");
+const image_input = document.querySelector("#image_input");
+let uploaded_image;
 
 //generate random colors
 function generateRandom() {
@@ -55,3 +57,16 @@ function copyHexColor() {
     document.colorize.removeChild(el);
   // as does execCommand 
 }
+
+// const image_input = document.querySelector("#image_input");
+// let uploaded_image;
+
+image_input.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+    uploaded_image = reader.result;
+    document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+});
+    reader.readAsDataURL(this.files[0]);
+});
+
